cf7e73b164474fd5d183a3ba6d88a57db9bf6c66
Никита Курилов <nvkurilov@edu.hse.ru>
2024-03-17 23:29:53 +0500

    New1

--- first_1/Program.cs
+++ first_1/Program.cs
@@ -19,7 +19,7 @@ namespace first_1
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                { 
                     webBuilder.UseStartup<Startup>();
                 });
     }
